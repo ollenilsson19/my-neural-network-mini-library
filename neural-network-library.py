9eb7ead34075332c85ba10b22f00f65e58ae2fdb
oen19 <oen19@ic.ac.uk>
2020-01-13 10:03:10 +0000

    added Xavier initialization function of network weights

--- neural-network-library.py
+++ neural-network-library.py
@@ -1,4 +1,15 @@
 
+import numpy as np
+
+
+
+def xavier_init(size, gain=1.0):
+    """
+    Xavier initialization of network weights.
+    """
+    low = -gain * np.sqrt(6.0 / np.sum(size))
+    high = gain * np.sqrt(6.0 / np.sum(size))
+    return np.random.uniform(low=low, high=high, size=size)
 
 
 class Layer:
