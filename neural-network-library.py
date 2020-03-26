dc2f510160a9010504d9b002bc6b2f1011a3cbd7
ollenilsson19 <ollenilsson@hotmail.se>
2020-03-26 15:16:10 +0000

    Linear layer class for MLP. Implemented forward and backward pass

--- neural-network-library.py
+++ neural-network-library.py
@@ -33,6 +33,82 @@ class Layer:
         pass
 
 
+
+class LinearLayer(Layer):
+    """
+    LinearLayer: Performs affine transformation of input.
+    """
+
+    def __init__(self, n_in, n_out):
+        """Constructor.
+
+        Arguments:
+            n_in {int} -- Number (or dimension) of inputs.
+            n_out {int} -- Number (or dimension) of outputs.
+        """
+        self.n_in = n_in
+        self.n_out = n_out
+
+        self._W = xavier_init((self.n_in, self.n_out))
+        self._b = xavier_init((1, self.n_out))
+
+        self._cache_current = None
+        self._grad_W_current = None
+        self._grad_b_current = None
+
+
+    def forward(self, x):#OK
+        """
+        Performs forward pass through the layer (i.e. returns Wx + b).
+
+        Logs information needed to compute gradient at a later stage in
+        `_cache_current`.
+
+        Arguments:
+            x {np.ndarray} -- Input array of shape (batch_size, n_in).
+
+        Returns:
+            {np.ndarray} -- Output array of shape (batch_size, n_out)
+        """
+        self.batch_size = len(x)
+        self._cache_current = x # save for gradient
+        return x@self._W + self._b
+
+
+    def backward(self, grad_z):
+        """
+        Given `grad_z`, the gradient of some scalar (e.g. loss) with respect to
+        the output of this layer, performs back pass through the layer (i.e.
+        computes gradients of loss with respect to parameters of layer and
+        inputs of layer).
+
+        Arguments:
+            grad_z {np.ndarray} -- Gradient array of shape (batch_size, n_out).
+
+        Returns:
+            {np.ndarray} -- Array containing gradient with repect to layer
+                input, of shape (batch_size, n_in).
+        """
+
+        self._grad_W_current = self._cache_current.T@grad_z #for updating parameters
+        self._grad_b_current = np.ones((1, self.batch_size))@grad_z # for updating parameters
+        return grad_z@self._W.T
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class MSELossLayer(Layer):
     """
     MSELossLayer: Computes mean-squared error between y_pred and y_target.
@@ -55,4 +131,3 @@ class MSELossLayer(Layer):
 
     def backward(self):
         return self._mse_grad(*self._cache_current)
-
